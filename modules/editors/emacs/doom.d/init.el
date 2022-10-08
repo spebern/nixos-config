717f7d86de59cfb62a4121b45ce04fa00e95c72d
Bernhard Specht <bernhard.specht@pentlandfirth.com>
2022-10-08 11:51:03 +0200

    enable nix for doom emacs

--- modules/editors/emacs/doom.d/init.el
+++ modules/editors/emacs/doom.d/init.el
@@ -144,7 +144,7 @@
        lua               ; one-based indices? one-based indices
        (markdown +grip)          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       ;;nix               ; I hereby declare "nix geht mehr!"
+       nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org +journal +jupyter +noter +roam2 +pomodoro +pandoc +dragndrop)               ; organize your plain life in plain text
        (php +lsp)               ; perl's insecure younger brother
