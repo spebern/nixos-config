3eae527ffe9c29d77fe0e11aa4c97d6aff62ff58
Bernhard Specht <bernhard.specht@pentlandfirth.com>
2022-10-09 14:01:37 +0200

    update sway config

--- modules/desktop/sway/home.nix
+++ modules/desktop/sway/home.nix
@@ -1,16 +1,3 @@
-#
-#  Sway Home manager configuration
-#
-#  flake.nix
-#   ├─ ./hosts
-#   │   └─ ./laptop
-#   │       └─ home.nix
-#   └─ ./modules
-#       └─ ./desktop
-#           └─ ./sway
-#               └─ home.nix *
-#
-
 { config, nixosConfig, lib, pkgs, ... }:
 
 let
@@ -27,17 +14,17 @@ in
 
       startup = [                                       # Run commands on Sway startup
         {command = "${pkgs.autotiling}/bin/autotiling"; always = true;} # Tiling Script
-        ##{command = ''
-        ##  ${pkgs.swayidle}/bin/swayidle -w \
-        ##      before-sleep '${pkgs.swaylock-fancy}/bin/swaylock-fancy'
-        ##''; always = true;}                             # Lock on lid close (currently disabled because using laptop as temporary server)
-        #{command = ''
-        #  ${pkgs.swayidle}/bin/swayidle \
-        #    timeout 120 '${pkgs.swaylock-fancy}/bin/swaylock-fancy' \
-        #    timeout 240 'swaymsg "output * dpms off"' \
-        #    resume 'swaymsg "output * dpms on"' \
-        #    before-sleep '${pkgs.swaylock-fancy}/bin/swaylock-fancy'
-        #''; always = true;}                            # Auto lock\
+        {command = ''
+         ${pkgs.swayidle}/bin/swayidle -w \
+             before-sleep '${pkgs.swaylock-fancy}/bin/swaylock-fancy'
+        ''; always = true;}                             # Lock on lid close (currently disabled because using laptop as temporary server)
+        {command = ''
+         ${pkgs.swayidle}/bin/swayidle \
+           timeout 120 '${pkgs.swaylock-fancy}/bin/swaylock-fancy' \
+           timeout 240 'swaymsg "output * dpms off"' \
+           resume 'swaymsg "output * dpms on"' \
+           before-sleep '${pkgs.swaylock-fancy}/bin/swaylock-fancy'
+        ''; always = true;}                            # Auto lock\
       ];
 
       bars = [];                                        # No bar because using Waybar
@@ -45,11 +32,6 @@ in
       fonts = {                                         # Font usedfor window tiles, navbar, ...
         names = [ "Source Code Pro" ];
         size = 10.0;
-      };
-
-      gaps = {                                          # Gaps for containters
-        inner = 5;
-        outer = 5;
       };
 
       input = {                                         # Input modules: $ man sway-input
@@ -86,11 +68,10 @@ in
         "${modifier}+Return" = "exec ${terminal}";      # Open terminal
 
         "${modifier}+d" = "exec ${menu}";           # Open menu
-        "${modifier}+e" = "exec ${pkgs.pcmanfm}/bin/pcmanfm"; # File Manager
         "${modifier}+l" = "exec ${pkgs.swaylock-fancy}/bin/swaylock-fancy"; # Lock Screen
 
         "${modifier}+r" = "reload";                     # Reload environment
-        "${modifier}+q" = "kill";                       # Kill container
+        "${modifier}+Shuft+q" = "kill";                       # Kill container
         "${modifier}+f" = "fullscreen toggle";          # Fullscreen
         "${modifier}+h" = "floating toggle";            # Floating
 
@@ -104,56 +85,44 @@ in
         "${modifier}+Shift+Up" = "move up";
         "${modifier}+Shift+Down" = "move down";
 
-        #"Alt+Left" = "workspace prev";                  # Navigate to previous or next workspace if it exists
-        #"Alt+Right" = "workspace next";
-        "Alt+Left" = "workspace prev_on_output";         # Navigate to previous or next workspace on output if it exists
-        "Alt+Right" = "workspace next_on_output";
+        "${modifier}+1" = "workspace number 1";
+        "${modifier}+2" = "workspace number 2";
+        "${modifier}+3" = "workspace number 3";
+        "${modifier}+4" = "workspace number 4";
+        "${modifier}+5" = "workspace number 5";
+        "${modifier}+6" = "workspace number 6";
+        "${modifier}+7" = "workspace number 7";
+        "${modifier}+8" = "workspace number 8";
+        "${modifier}+9" = "workspace number 9";
+        "${modifier}+0" = "workspace number 10";
 
-        "Alt+1" = "workspace number 1";                 # Open workspace x
-        "Alt+2" = "workspace number 2";
-        "Alt+3" = "workspace number 3";
-        #"Alt+4" = "workspace number 4";
-        #"Alt+5" = "workspace number 5";
-
-        "Alt+Shift+Left" = "move container to workspace prev, workspace prev";    # Move container to next available workspace and focus
-        "Alt+Shift+Right" = "move container to workspace next, workspace next";
-
-        "Alt+Shift+1" = "move container to workspace number 1";     # Move container to specific workspace
-        "Alt+Shift+2" = "move container to workspace number 2";
-        "Alt+Shift+3" = "move container to workspace number 3";
-        "Alt+Shift+4" = "move container to workspace number 4";
-        "Alt+Shift+5" = "move container to workspace number 5";
+        "${modifier}+Shift+1" = "move container to workspace number 1";
+        "${modifier}+Shift+2" = "move container to workspace number 2";
+        "${modifier}+Shift+3" = "move container to workspace number 3";
+        "${modifier}+Shift+4" = "move container to workspace number 4";
+        "${modifier}+Shift+5" = "move container to workspace number 5";
+        "${modifier}+Shift+6" = "move container to workspace number 6";
+        "${modifier}+Shift+7" = "move container to workspace number 7";
+        "${modifier}+Shift+8" = "move container to workspace number 8";
+        "${modifier}+Shift+9" = "move container to workspace number 9";
+        "${modifier}+Shift+0" = "move container to workspace number 10";
 
         "Control+Up" = "resize shrink height 20px";     # Resize container
         "Control+Down" = "resize grow height 20px";
         "Control+Left" = "resize shrink width 20px";
         "Control+Right" = "resize grow width 20px";
 
-        "Print" = "exec ${pkgs.flameshot}/bin/flameshot gui"; # Screenshots
-
         "XF86AudioLowerVolume" = "exec ${pkgs.pamixer}/bin/pamixer -d 10";   #Volume control
         "XF86AudioRaiseVolume" = "exec ${pkgs.pamixer}/bin/pamixer -i 10";
         "XF86AudioMute" = "exec ${pkgs.pamixer}/bin/pamixer -t";             #Media control
         "XF86AudioMicMute" = "exec ${pkgs.pamixer}/bin/pamixer --default-source -t";
-        #"XF86AudioPlay" = "exec ${pkgs.playerctl}/bin/playerctl play-pause";
-        #"XF86AudioNext" = "exec ${pkgs.playerctl}/bin/playerctl next";
-        #"XF86AudioPrev" = "exec ${pkgs.playerctl}/bin/playerctl previous";
-        #
         "XF86MonBrightnessDown" = "exec ${pkgs.light}/bin/light -U  5";      # Display brightness control
         "XF86MonBrightnessUp" = "exec ${pkgs.light}/bin/light -A 5";
       };
     };
     extraConfig = ''
-      set $opacity 0.8
-      for_window [class=".*"] opacity 0.95
-      for_window [app_id=".*"] opacity 0.95
-      for_window [app_id="pcmanfm"] opacity 0.95, floating enable
-      for_window [app_id="Alacritty"] opacity $opacity
-      for_window [title="drun"] opacity $opacity
-      for_window [class="Emacs"] opacity $opacity
       for_window [app_id="pavucontrol"] floating enable, sticky
       for_window [app_id=".blueman-manager-wrapped"] floating enable
-      for_window [title="Picture in picture"] floating enable, move position 1205 634, resize set 700 400, sticky enable
     '';                                    # $ swaymsg -t get_tree or get_outputs
     extraSessionCommands = ''
       #export WLR_NO_HARDWARE_CURSORS="1";  # Needed for cursor in vm
