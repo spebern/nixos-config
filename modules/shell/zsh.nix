02a50286667a9bf28fc54ea6377d6403bac4742e
Bernhard Specht <bernhard.specht@pentlandfirth.com>
2022-10-09 12:29:44 +0200

    update zsh config

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -1,7 +1,3 @@
-#
-# Shell
-#
-
 { pkgs, ... }:
 
 {
@@ -9,25 +5,18 @@
     zsh = {
       enable = true;
       dotDir = ".config/zsh_nix";
-      enableAutosuggestions = true;             # Auto suggest options and highlights syntact, searches in history for options
+      enableAutosuggestions = true;
       enableSyntaxHighlighting = true;
       history.size = 10000;
 
-      oh-my-zsh = {                             # Extra plugins for zsh
+      oh-my-zsh = {
         enable = true;
         plugins = [ "git" ];
         custom = "$HOME/.config/zsh_nix/custom";
       };
 
-      initExtra = ''                            # Zsh theme
-        # Spaceship
-        source ${pkgs.spaceship-prompt}/share/zsh/site-functions/prompt_spaceship_setup
+      initExtra = ''
         autoload -U promptinit; promptinit
-#       source $HOME/.config/shell/shell_init
-        # Hook direnv
-#       emulate zsh -c "$(direnv hook zsh)"
-        # Swag
-        pfetch                                  # Show fetch logo on terminal start
       '';
     };
   };
